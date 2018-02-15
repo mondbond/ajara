445364539fc78798eb6187bdf22ccc3deef01390
ibahr <vanya0794@gmail.com>
2018-02-15 11:09:01 +0200

    added review and validator bean to git

---
+++ src/main/java/controllers/ValidatorBean.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import entity.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import repository.BookFacade;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+@ManagedBean(name = "validatorBean")
+@ViewScoped
+public class ValidatorBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValidatorBean.class);
+
+    @EJB
+    private BookFacade bookFacade;
+
+    public void validateISBN(FacesContext context, UIComponent component,
+                             Object value) throws ValidatorException {
+        LOGGER.info("validateISBN [{}]", value.toString());
+
+        if(!value.toString().matches("[0-9]{10,18}")){
+            FacesMessage msg =
+                    new FacesMessage("ISBN must contain minimum 10, maximum 18 numbers",
+                            "ISBN must contain minimum 10, maximum 18 numbers");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }else if (!bookFacade.isUnique(Book.getISBN_COLUMN(), value.toString())){
+            FacesMessage msg =
+                    new FacesMessage("ISBN is not unique",
+                            "ISBN is not unique");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+    }
+
+
+
+}
---
+++ src/main/java/entity/Review.java
@@ -0,0 +1,62 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import entity.listeners.CreateDateListener;
+import entity.listeners.HasDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners({CreateDateListener.class})
+@Table(name = "REVIEWS")
+@Data
+public class Review implements Serializable, HasDate {
+
+    public static @Getter final String PK_COLUMN = "ID";
+    public static @Getter final String NAME_COLUMN = "FIRST_NAME";
+    public static @Getter final String AVG_RATING_COLUMN = "AVG_RATING";
+    public static @Getter final String DATE_COLUMN = "CREATE_DATE";
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="review_id_sequence")
+    @SequenceGenerator(name="review_id_sequence", allocationSize = 1, sequenceName="REVIEW_PK_SEQ")
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "COMMENTER_NAME")
+    @Pattern(regexp = "[a-zA-Z0-9- ]{3,100}", message = "Name must contain minimum 3 maximum 100 characters without special symbols")
+    private String commenterName;
+
+    @Column(name = "COM", nullable = true)
+    private String com;
+
+    @Column(name = "RATING")
+    @Range(min = 0, max = 5, message = "Rating must be between 0 and 5")
+    @NotNull
+    private Integer rating;
+
+    @Column(name = "CREATE_DATE")
+    private LocalDateTime createDate;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "ID_BOOK")
+    private Book book;
+
+    @Override
+    public void setDate(LocalDateTime date) {
+        createDate = date;
+    }
+}
+
